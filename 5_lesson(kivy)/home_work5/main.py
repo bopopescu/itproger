ce7bf84b86a93d41065394e2b1d65bf728aa19f0
duk1edev <duk1e.ptc.ua@yandex.ru>
2020-02-15 12:25:00 +0200

    added filter input

--- 5_lesson(kivy)/home_work5/main.py
+++ 5_lesson(kivy)/home_work5/main.py
@@ -1,11 +1,7 @@
-from PIL.Image import eval
 from kivy.uix.boxlayout import BoxLayout
-from kivy.config import Config
 from kivy.core.window import Window
 from kivymd.app import MDApp
-from kivy.app import App
 from kivy.properties import ObjectProperty
-from kivy.properties import BooleanProperty
 
 Window.size = (360, 640)
 
@@ -15,11 +11,19 @@ class Container(BoxLayout):
     button_mul = ObjectProperty()
     reset = ObjectProperty()
     buffer = ObjectProperty()
-    clear_bool = BooleanProperty()
+    id7 = ObjectProperty()
 
     number = 0
-    sign = ""
     buffer = 0
+    zero_have = False
+    sign = ""
+
+    # Проверка ввода  - количесво значений введенных в TextInput не может быть больше 10  - 1234567890
+    def check_input(self, text):
+        if len(self.get_input.text) > 9:
+            self.get_input.text = self.get_input.text[:10:]
+        else:
+            self.get_input.text += text
 
     def print_operation(self, number, operator):
         self.sign = operator
@@ -28,45 +32,54 @@ class Container(BoxLayout):
 
     def clear_display(self):
         self.get_input.text = ''
-        self.clear_bool = False
 
     def do_operation(self, number2):
-        print("я buffer do = ", self.buffer)
-
-        if self.sign == "+":
-            print(self.sign)
-            self.buffer = float(self.number) + float(number2)
-            self.get_input.text = str(self.buffer)
-
-        elif self.sign == "-":
-            print(self.sign)
-            self.buffer = float(self.number) - float(number2)
-            self.get_input.text = str(self.buffer)
-
-        elif self.sign == "*":
-            print(self.sign)
-            self.buffer = float(self.number) * float(number2)
-            self.get_input.text = str(self.buffer)
-        elif self.sign == "/":
-            try:
-                print(self.sign)
-                self.buffer = float(self.number) / float(number2)
+        if number2 == "" or number2 == "Error":
+            self.get_input.text = "0"
+            self.clear_display()
+        else:
+            # print("я buffer do = ", self.buffer)
+            if self.sign == "+":
+                self.buffer = round((float(self.number) + float(number2)), 10)
                 self.get_input.text = str(self.buffer)
-            except ZeroDivisionError:
-                self.get_input.text = "Error"
 
-        print("я buffer posle = ", self.buffer)
+            elif self.sign == "-":
+                self.buffer = round((float(self.number) - float(number2)), 10)
+                self.get_input.text = str(self.buffer)
 
+            elif self.sign == "*":
+                self.buffer = round((float(self.number) * float(number2)), 8)
+                self.get_input.text = str(self.buffer)
+
+            elif self.sign == "/":
+                try:
+                    self.buffer = round((float(self.number) / float(number2)), 8)
+                    self.get_input.text = str(self.buffer)
+                except ZeroDivisionError:
+                    self.get_input.text = "Error"
+        # print("buffer after = ", self.buffer)
+
+    # Ввод точки для чисел
     def decimal_sign(self):
-        if self.get_input.text[0] != 0 and '.' not in self.get_input.text:
+        if len(self.get_input.text) != 0 and '.' not in self.get_input.text:
             self.get_input.text += "."
+        else:
+            self.get_input.text = self.get_input.text
 
+    # Добавлена возможность ввести ноль после  - "." например число 0.001234
+    def enter_zero(self, text):
+        self.get_input.text = '0'
+        if self.get_input.text[0] == '0':
+            print("ноьл туту")
+            print(self.get_input.text)
+        elif self.get_input.text[0] and self.get_input.text[1] == ".":
+            print("работал этот участо",self.get_input.text)
 
 class CalculatorApp(MDApp):
     title = "Calculator"
 
     def __init__(self, **kwargs):  # Вызываем self конструктор, а затем ...
-        self.theme_cls.theme_style = "Dark"
+        self.theme_cls.theme_style = "Light"
         super().__init__(**kwargs)  # вызываем конструктор из базоваого MDApp
 
     def build(self):
@@ -75,13 +88,3 @@ class CalculatorApp(MDApp):
 
 if __name__ == "__main__":
     CalculatorApp().run()
-
-# 1.  Выполнить ввод большего числа
-# 2.  Ввод числа с запятой( если нет числа в начале не принимать ввод)
-#     и что бы точка была только одна
-# 3.  Выполнить сложение двух чисел
-#
-#
-#
-#
-#
